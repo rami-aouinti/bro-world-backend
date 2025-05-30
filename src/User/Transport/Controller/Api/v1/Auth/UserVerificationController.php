f669bd7a64dd50b148fc738327146475f5d9c332
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-05-30 03:14:30 +0200

    Registration Controller

---
+++ src/User/Transport/Controller/Api/v1/Auth/UserVerificationController.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Transport\Controller\Api\v1\Auth;
+
+use App\User\Domain\Repository\Interfaces\UserRepositoryInterface;
+use Doctrine\ORM\Exception\NotSupported;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use OpenApi\Attributes as OA;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * @package App\User\User
+ */
+#[AsController]
+#[OA\Tag(name: 'Authentication')]
+readonly class UserVerificationController
+{
+    public function __construct(
+        private UserRepositoryInterface $userRepository,
+    ) {
+    }
+
+    /**
+     * Get user Json Web Token (JWT) for authentication.
+     *
+     * @throws NotSupported
+     * @throws ORMException
+     */
+    #[Route(
+        path: '/v1/auth/verification_email',
+        methods: [Request::METHOD_POST],
+    )]
+    public function __invoke(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $token = $data['token'] ?? null;
+
+        if (!$token) {
+            return new JsonResponse([
+                'message' => 'Token empty',
+            ], 400);
+        }
+
+        $user = $this->userRepository->findOneBy([
+            'verificationToken' => $token,
+        ]);
+
+        if (!$user) {
+            return new JsonResponse([
+                'message' => 'Token not valid or expired.',
+            ], 400);
+        }
+
+        $user->setVerificationToken(null);
+
+        try {
+            $this->userRepository->save($user);
+        } catch (OptimisticLockException $e) {
+        } catch (ORMException $e) {
+            throw new ORMException($e->getMessage());
+        }
+
+        return new JsonResponse([
+            'message' => 'Email verified successfully',
+        ], 200);
+    }
+}
