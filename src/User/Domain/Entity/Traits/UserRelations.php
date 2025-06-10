7fbfa8a00704815be6ac8109f0e5667bc31d70fc
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-10 22:57:24 +0200

    Ecommerce Installation

--- src/User/Domain/Entity/Traits/UserRelations.php
+++ src/User/Domain/Entity/Traits/UserRelations.php
@@ -24,6 +24,7 @@ trait UserRelations
 {
     #[ORM\OneToOne(mappedBy: 'user', targetEntity: UserProfile::class, cascade: ['persist', 'remove'])]
     #[Groups([
+        'User',
         'User.profile',
 
         User::SET_USER_PROFILE,
