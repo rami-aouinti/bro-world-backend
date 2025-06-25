b5aae59e96789d42516ef806b3c75978d9a19993
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-26 00:22:04 +0200

    Plugin Install

--- src/Tool/Transport/Controller/Api/PluginController.php
+++ src/Tool/Transport/Controller/Api/PluginController.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Tool\Transport\Controller\Api;
 
+use App\General\Domain\Utils\JSON;
 use App\User\Domain\Entity\Plugin;
+use App\User\Domain\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use OpenApi\Attributes as OA;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class PluginController
@@ -23,16 +28,30 @@ readonly class PluginController
 {
 
     public function __construct(
+        private readonly SerializerInterface $serializer,
         private EntityManagerInterface $em
     )
     {
     }
 
+    /**
+     * @throws ExceptionInterface
+     * @throws JsonException
+     */
     #[Route('/plugins', name: 'api_plugins_list', methods: ['GET'])]
     public function __invoke(): JsonResponse
     {
         $plugins = $this->em->getRepository(Plugin::class)->findAll();
-
-        return new JsonResponse($plugins);
+        $output = JSON::decode(
+            $this->serializer->serialize(
+                $plugins,
+                'json',
+                [
+                    'groups' => User::SET_USER_PROFILE,
+                ]
+            ),
+            true,
+        );
+        return new JsonResponse($output);
     }
 }
