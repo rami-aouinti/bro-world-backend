9b878d9061bd51ac5ea7a89370427106df2979f1
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-02 14:53:25 +0200

    sadaada

--- src/User/Transport/Controller/Api/v1/Profile/StoryController.php
+++ src/User/Transport/Controller/Api/v1/Profile/StoryController.php
@@ -8,6 +8,8 @@ use App\General\Domain\Utils\JSON;
 use App\User\Application\Service\NotificationService;
 use App\User\Application\Service\UserService;
 use App\User\Domain\Entity\User;
+use App\User\Domain\Repository\Interfaces\StoryRepositoryInterface;
+use Closure;
 use JsonException;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,6 +21,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Throwable;
 
 /**
@@ -32,7 +35,8 @@ readonly class StoryController
         private CacheInterface $userCache,
         private SerializerInterface $serializer,
         private UserService $userService,
-        private NotificationService $notificationService
+        private NotificationService $notificationService,
+        private StoryRepositoryInterface $storyRepository
     ) {
     }
 
@@ -54,14 +58,16 @@ readonly class StoryController
     #[IsGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)]
     public function __invoke(User $loggedInUser, Request $request): JsonResponse
     {
-        $cacheKey = 'stories_users_' . $loggedInUser->getId();
-        $this->userCache->delete($cacheKey);
         $story = $this->userService->uploadStory($loggedInUser, $request);
         $this->notificationService->createNotificationStory(
             $request->headers->get('Authorization'),
             'PUSH',
             $story
         );
+        $cacheKey = 'stories_users_' . $loggedInUser->getId();
+        $this->userCache->delete($cacheKey);
+        $this->userCache->get($cacheKey, fn (ItemInterface $item) => $this->getClosure($loggedInUser)($item));
+
         /** @var array<string, string|array<string, string>> $output */
         $output = JSON::decode(
             $this->serializer->serialize(
@@ -75,5 +81,20 @@ readonly class StoryController
         );
 
         return new JsonResponse($output);
+    }
+
+    /**
+     *
+     * @param User $loggedInUser
+     *
+     * @return Closure
+     */
+    private function getClosure(User $loggedInUser): Closure
+    {
+        return function (ItemInterface $item) use ($loggedInUser): array {
+            $item->expiresAfter(31536000);
+
+            return $this->storyRepository->availableStories($loggedInUser);
+        };
     }
 }
