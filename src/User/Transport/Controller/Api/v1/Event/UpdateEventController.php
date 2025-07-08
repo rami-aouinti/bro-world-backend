85b4f158a7ef9910a2910716e553a3e1ec719880
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-08 05:02:13 +0200

    sadaada

--- src/User/Transport/Controller/Api/v1/Event/UpdateEventController.php
+++ src/User/Transport/Controller/Api/v1/Event/UpdateEventController.php
@@ -53,26 +53,8 @@ readonly class UpdateEventController
     #[IsGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)]
     public function __invoke(User $loggedInUser, Event $event, Request $request): JsonResponse
     {
-        $event->setTitle($request->request->get('title', $event->getTitle()));
-        $event->setStart(new DateTimeImmutable($request->request->get('start', $event->getStart()->format('Y-m-d H:i:s'))));
-        $event->setEnd(
-            $request->request->has('end')
-                ? new DateTimeImmutable($request->request->get('end', $event->getEnd()?->format('Y-m-d H:i:s')))
-                : null
-        );
-        $event->setColor($request->request->get('color', $event->getColor()));
-        $event->setAllDay(
-            $request->request->has('allDay')
-                ? (bool)$request->request->get('allDay', $event->isAllDay())
-                : $event->isAllDay()
-        );
-        $event->setDescription($request->request->get('description', $event->getDescription()));
-        $event->setLocation($request->request->get('location', $event->getLocation()));
-        $event->setIsPrivate(
-            $request->request->has('isPrivate')
-                ? (bool)$request->request->get('isPrivate', $event->isPrivate())
-                : $event->isPrivate()
-        );
+        $data = $request->request->all();
+        $event->setTitle($data['title'] ?? $event->getTitle());
         $this->repository->save($event);
         $output = JSON::decode(
             $this->serializer->serialize(
