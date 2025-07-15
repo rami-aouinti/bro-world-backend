bfc7c094fc3f5fc9f5860d34047403663d95a36f
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-15 17:47:47 +0200

    add fehlende Temporal Config Tags

---
+++ src/User/Transport/Controller/Api/v1/Profile/UpdatePasswordController.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Transport\Controller\Api\v1\Profile;
+
+use App\General\Domain\Utils\JSON;
+use App\Role\Application\Security\Interfaces\RolesServiceInterface;
+use App\User\Application\Resource\UserResource;
+use App\User\Application\Service\UserService;
+use App\User\Domain\Entity\User;
+use App\User\Domain\Entity\UserProfile;
+use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Property;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Throwable;
+
+/**
+ * @package App\User
+ */
+#[AsController]
+#[OA\Tag(name: 'Profile')]
+readonly class UpdatePasswordController
+{
+    public function __construct(
+        private SerializerInterface $serializer,
+        private RolesServiceInterface $rolesService,
+        private EntityManagerInterface $entityManager,
+        private UserService $userService,
+        private UserResource $userResource
+    ) {
+    }
+
+    /**
+     * Get current user profile data, accessible only for 'IS_AUTHENTICATED_FULLY' users.
+     *
+     * @param User    $loggedInUser
+     * @param Request $request
+     *
+     * @throws JsonException
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws ExceptionInterface
+     * @throws Throwable
+     * @return JsonResponse
+     */
+    #[Route(
+        path: '/v1/profile/password/update',
+        methods: [Request::METHOD_POST],
+    )]
+    #[IsGranted(AuthenticatedVoter::IS_AUTHENTICATED_FULLY)]
+    public function __invoke(User $loggedInUser, Request $request): JsonResponse
+    {
+        $user = $request->request->all();
+
+        if(isset($user['oldPassword'], $user['newPassword'], $user['newRepeatPassword'])) {
+            if (($loggedInUser->getPlainPassword() === $user['oldPassword']) &&
+                $user['newPassword'] === $user['newRepeatPassword']
+            ) {
+                $loggedInUser->setPlainPassword($user['newPassword']);
+            }
+        }
+
+        $this->userResource->save($loggedInUser);
+
+        /** @var array<string, string|array<string, string>> $output */
+        $output = JSON::decode(
+            $this->serializer->serialize(
+                $loggedInUser,
+                'json',
+                [
+                    'groups' => User::SET_USER_PROFILE,
+                ]
+            ),
+            true,
+        );
+        /** @var array<int, string> $roles */
+        $roles = $output['roles'];
+        $output['roles'] = $this->rolesService->getInheritedRoles($roles);
+
+        return new JsonResponse($output);
+    }
+}
