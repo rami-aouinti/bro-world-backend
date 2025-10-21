e8c3ab7cea8c67897c2b97e053379dbb12295f5d
Rami <rami.aouinti@gmail.com>
2025-10-21 19:23:26 +0200

    Remove MongoDB APM loggers when driver missing

---
+++ src/General/Application/Compiler/DisableDoctrineMongoCommandLoggerPass.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\General\Application\Compiler;
+
+use Doctrine\Bundle\MongoDBBundle\APM\PSRCommandLogger;
+use Doctrine\Bundle\MongoDBBundle\APM\StopwatchCommandLogger;
+use MongoDB\Driver\Monitoring\CommandSubscriber;
+use Override;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * @package App\General
+ */
+class DisableDoctrineMongoCommandLoggerPass implements CompilerPassInterface
+{
+    #[Override]
+    public function process(ContainerBuilder $container): void
+    {
+        if (interface_exists(CommandSubscriber::class)) {
+            return;
+        }
+
+        $classesToRemove = [
+            StopwatchCommandLogger::class,
+            PSRCommandLogger::class,
+        ];
+
+        $serviceIdsToRemove = [];
+
+        /** @var Definition $definition */
+        foreach ($container->getDefinitions() as $id => $definition) {
+            if ($definition->getClass() !== null && in_array($definition->getClass(), $classesToRemove, true)) {
+                $serviceIdsToRemove[] = $id;
+            }
+        }
+
+        foreach ($serviceIdsToRemove as $id) {
+            $container->removeDefinition($id);
+        }
+
+        foreach ($container->getAliases() as $id => $alias) {
+            if (in_array((string) $alias, $serviceIdsToRemove, true)) {
+                $container->removeAlias($id);
+            }
+        }
+    }
+}
